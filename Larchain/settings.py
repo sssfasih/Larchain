781bba94d41b263da62b0d51a885ac7b58d45315
Syed Fasih Uddin <sssfasih@gmail.com>
2023-05-14 16:13:39 +0500

    initial code for login signup logout

--- Larchain/settings.py
+++ Larchain/settings.py
@@ -113,6 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'scanner.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- scanner/urls.py
+++ scanner/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
 
 ]
